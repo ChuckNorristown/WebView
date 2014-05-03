d104db613f09f9714182ab0b929898af877b9bb8
Sean Shields <sean@dmgctrl.com>
2014-05-03 08:51:48 -0400

    initial commit

---
+++ WebView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WebView
+//
+//  Created by Sean Shields on 5/1/14.
+//  Copyright (c) 2014 Sean Shields. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WebView/ViewController.m
@@ -0,0 +1,33 @@
+//
+//  ViewController.m
+//  WebView
+//
+//  Created by Sean Shields on 5/1/14.
+//  Copyright (c) 2014 Sean Shields. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    NSString* htmlFile = [[NSBundle mainBundle] pathForResource:@"index" ofType:@"html" inDirectory:@"CommunityKit"];
+    
+//    NSString *htmlFile = [[NSBundle mainBundle] pathForResource:@"index" ofType:@"html"];
+    NSString* htmlString = [NSString stringWithContentsOfFile:htmlFile encoding:NSUTF8StringEncoding error:nil];
+    [_viewWeb loadHTMLString:htmlString baseURL:[NSURL fileURLWithPath:[htmlFile stringByDeletingLastPathComponent] isDirectory:YES]];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
